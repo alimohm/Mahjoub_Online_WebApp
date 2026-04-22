e21389f6971f7d3418bb429fa75a3afb73aa6105
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-22 14:40:09 +0300

    Update routes.py

--- admin_panel/routes.py
+++ admin_panel/routes.py
@@ -1,12 +1,12 @@
 from flask import Blueprint, render_template
 
-# تعريف بوابة الإدارة
 admin_bp = Blueprint('admin', __name__, 
-                     template_folder='templates',
+                     template_folder='templates', # يشير إلى admin_panel/templates
                      static_folder='static')
 
 @admin_bp.route('/admin/login')
 def login():
+    # هنا Flask سيبحث داخل admin_panel/templates/admin/login.html
     return render_template('admin/login.html')
 
 @admin_bp.route('/admin/dashboard')
@@ -16,3 +16,11 @@ def dashboard():
 @admin_bp.route('/admin/wallets')
 def wallets():
     return render_template('admin/wallets.html')
+
+@admin_bp.route('/admin/product-review')
+def product_review():
+    return render_template('admin/product_review.html')
+
+@admin_bp.route('/admin/order-routing')
+def order_routing():
+    return render_template('admin/order_routing.html')
