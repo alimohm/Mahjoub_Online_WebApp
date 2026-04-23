1b0a94ebdb73073647275adf5a16df9a17e0b38d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-23 16:43:24 +0300

    Update __init__.py

--- core/__init__.py
+++ core/__init__.py
@@ -1,23 +1,22 @@
 from flask import Flask
 from config import Config
-from .models import db # استدعاء db من ملف models.py
+from .models import db
 
 def create_app(config_class=Config):
     app = Flask(__name__)
     app.config.from_object(config_class)
 
-    # تهيئة db مع التطبيق
     db.init_app(app)
 
     with app.app_context():
-        # استدعاء وتسجيل المسارات
+        # تسجيل الصفحات (Blueprints)
         from admin_panel.routes import admin_bp
         from supplier_panel.routes import supplier_bp
-        
         app.register_blueprint(admin_bp, url_prefix='/admin')
         app.register_blueprint(supplier_bp, url_prefix='/supplier')
 
-        # إنشاء الجداول في قاعدة البيانات (مثل PostgreSQL على Railway)
+        # إنشاء الجداول فوراً عند تشغيل السيرفر
         db.create_all()
+        print("--- ✅ Database Connected & Tables Created ---")
 
     return app
