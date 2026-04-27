9eaf47f6a3e5fe894ef76c7eccbd1bd850964c02
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 15:29:02 +0300

    Refactor supplier blueprint initialization

--- supplier_panel/__init__.py
+++ supplier_panel/__init__.py
@@ -1,14 +1,7 @@
 from flask import Blueprint
 
-# إنشاء البلوبرنت الخاص بقطاع الموردين (The Supplier Sector)
-# تم تحديد مجلد القوالب 'templates' لضمان قراءة التصاميم السيادية
-supplier_bp = Blueprint(
-    'supplier_panel', 
-    __name__, 
-    template_folder='templates',
-    static_folder='static'
-)
+# تعريف البلوبرنت
+supplier_bp = Blueprint('supplier_panel', __name__, template_folder='templates')
 
-# ملاحظة حاسمة: يجب استيراد routes في الأسفل لتجنب مشكلة (Circular Import)
-# هذا الاستيراد هو الذي يربط المسارات (الدخول، الداشبورد، الترسانة) بالبلوبرنت
+# استيراد المسارات لربطها بالبلوبرنت
 from . import routes
