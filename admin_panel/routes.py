ad500687a1bc9894bb283bb05ff33a389638b604
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 23:33:35 +0300

    Update routes.py

--- admin_panel/routes.py
+++ admin_panel/routes.py
@@ -1,6 +1,6 @@
 from flask import Blueprint
 
-# تعريف البوابة مع تحديد مجلد القوالب
+# هذا السطر هو الذي "يعمد" المجلد كبوابة إدارة
 admin_bp = Blueprint('admin_panel', __name__, template_folder='templates')
 
 from . import routes
