b2aebe86c203320e764cc5fd4835d8d391ae7ca6
alimohm <69640611+alimohm@users.noreply.github.com>
2026-04-27 12:12:27 +0300

    Update user.py

--- core/models/user.py
+++ core/models/user.py
@@ -1,14 +1,11 @@
 from core import db
-from datetime import datetime
 from flask_login import UserMixin
 
 class User(db.Model, UserMixin):
-    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80), unique=True, nullable=False)
-    password = db.Column(db.String(200), nullable=False)
-    role = db.Column(db.String(20), default='admin') # 'admin' للتحكم المطلق
-    created_at = db.Column(db.DateTime, default=datetime.utcnow)
-
-    def __repr__(self):
-        return f'<Admin: {self.username}>'
+    username = db.Column(db.String(150), unique=True, nullable=False)
+    password = db.Column(db.String(256), nullable=False)
+    role = db.Column(db.String(20), default='supplier')  # 'admin' أو 'supplier'
+    
+    # ربط المستخدم ببيانات المورد إذا كان دوره مورد
+    supplier_profile = db.relationship('Supplier', backref='user', uselist=False)
