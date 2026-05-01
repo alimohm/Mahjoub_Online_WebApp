a31d2b80f6bd134f83c02217c786656e11a1f93c
alimohm <69640611+alimohm@users.noreply.github.com>
2026-05-01 20:01:46 +0300

    Update __init__.py

--- admin_panel/__init__.py
+++ admin_panel/__init__.py
@@ -1,7 +1,5 @@
 from flask import Blueprint
 
-# تعريف البلوبرنت المركزي للإدارة والتحكم
-# تم ضبط الاسم 'admin' ليتوافق مع حوكمة المسارات في النواة
 admin_bp = Blueprint(
     'admin', 
     __name__, 
@@ -9,6 +7,4 @@ admin_bp = Blueprint(
     static_folder='static'
 )
 
-# استيراد المنطق (Logic) الخاص بالدخول والتحقق
-# يتم الاستيراد هنا لضمان ربط الدوال بالبلوبرنت بعد تعريفه
 from . import routes
