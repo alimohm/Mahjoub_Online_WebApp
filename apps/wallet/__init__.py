7521da45cb12f16c6d645ce3ce244e03be0c6879
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-22 18:35:02 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -3,7 +3,9 @@ from flask import Blueprint
 
 # هذا التعريف هو الأساس لربط الروابط. 
 # يجب أن يكون الاسم 'wallet' مطابقاً لما هو مسجل في apps/__init__.py
+from flask import Blueprint
 wallet_bp = Blueprint('wallet', __name__, template_folder='templates')
+from . import routes
 
 # هذا السطر ضروري جداً لكي يتعرف التطبيق على الدوال المعرفة في ملف routes.py
 from . import routes
