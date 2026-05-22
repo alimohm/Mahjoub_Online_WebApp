83b400ad159ff283bcf52de80f8b8504576fa54e
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-22 17:09:34 +0300

    Update __init__.py

--- apps/admin_dashboard/__init__.py
+++ apps/admin_dashboard/__init__.py
@@ -1,12 +1,14 @@
 # coding: utf-8
 from flask import Blueprint
 
-# تعريف البلوبرينت
+# تعريف البلوبرينت (Blueprint) الخاص بلوحة القيادة
+# هذا هو الذي يتم استيراده في المصنع المركزي (apps/__init__.py)
 admin_dashboard_bp = Blueprint(
     'admin_dashboard', 
     __name__, 
     template_folder='templates'
 )
 
-# استيراد المسارات بعد تعريف البلوبرينت لمنع الخطأ
+# استيراد المسارات (Routes) هنا بعد تعريف البلوبرينت 
+# لتجنب خطأ الاستيراد الدائري (Circular Import Error)
 from . import routes
