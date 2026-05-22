dff542b4abd723fde9241b841c6405ee35515a7e
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-22 18:54:52 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,8 +1,5 @@
-# coding: utf-8
 from flask import Blueprint
 
-# تعريف البلوبرينت باسم 'wallet' ليتطابق مع ما تم تسجيله في apps/__init__.py
 wallet_bp = Blueprint('wallet', __name__, template_folder='templates')
 
-# استيراد ملف المسارات لضمان تسجيلها داخل البلوبرينت
 from . import routes
