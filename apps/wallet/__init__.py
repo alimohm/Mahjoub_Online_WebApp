f2ba43f16e7c56edfe10892b6e30c757903cd16f
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-22 16:47:41 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,12 +1,5 @@
-# coding: utf-8
 from flask import Blueprint
 
-# 👑 تعريف البلوبرينت
-admin_wallet = Blueprint(
-    'admin_wallet', 
-    __name__, 
-    template_folder='templates'
-)
+wallet_bp = Blueprint('wallet', __name__)
 
-# 🛡️ استيراد المسارات هنا (هذا صحيح طالما أنك لا تستورد 'db' أو 'models' داخل routes.py في مستوى الملف العلوي)
 from . import routes
