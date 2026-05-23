62681619ac0fc7253e28e1ade2a8e92c41e68eeb
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-23 20:39:51 +0300

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,7 +1,8 @@
 # coding: utf-8
 # تجميع وإشهار النماذج لسهولة الاستيراد من خارج حزمة models
 
-from apps.models.admin_db import AdminUser, Supplier
+from apps.models.admin_db import AdminUser
+from apps.models.supplier_db import Supplier  # استدعاء الموردين من ملفها المستقل والصحيح ✅
 
 # أي موديلات جديدة تقوم بإنشائها مستقبلاً في ملفات أخرى، قم بإشهارها هنا كالتالي:
 # from apps.models.products import Product
