30f697502d66fc46456f33094182d0c6b6eaac7b
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-24 13:54:57 +0300

    Update __init__.py

--- apps/add_supplier/__init__.py
+++ apps/add_supplier/__init__.py
@@ -1,12 +1,13 @@
 # coding: utf-8
 from flask import Blueprint
 
-# تعريف الـ Blueprint بالاسم المعتمد في الـ url_for لمنع تكرار أخطاء البناء
+# تعريف الـ Blueprint بالاسم المعتمد 'add_supplier' الحاكم لعمليات الـ url_for
 admin_suppliers_bp = Blueprint(
     'add_supplier', 
     __name__, 
-    template_folder='templates'
+    template_folder='templates',
+    static_folder='static'  # تأمين مسار الملفات الساكنة الخاصة بالموديل عند الحاجة
 )
 
-# استدعاء المسارات (Routes) لربطها بالـ Blueprint بعد تعريفه لتجنب الـ Circular Import
+# استدعاء المسارات (Routes) بشكل سفلي لربطها بالـ Blueprint بعد تعريفه لتجنب الـ Circular Import
 from . import routes
