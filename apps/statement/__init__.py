23dc6d5fc281924e551582d7d066c6ed624055af
Ali Mahjoub <alyasserandsonsfoodstuffs@gmail.com>
2026-05-25 16:36:43 +0300

    Update __init__.py

--- apps/statement/__init__.py
+++ apps/statement/__init__.py
@@ -1,7 +1,8 @@
 from flask import Blueprint
 
+# تأكد أن الاسم هنا هو 'statement_blueprint'
 statement_blueprint = Blueprint(
-    'statement_blueprint',  # تأكد أن هذا الاسم هو 'statement_blueprint'
+    'statement_blueprint', 
     __name__, 
     template_folder='templates'
 )
