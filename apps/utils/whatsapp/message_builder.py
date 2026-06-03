9a5f3b26265f5dcb184f86022f8fcebf8c6c1a5d
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-03 18:03:18 +0300

    Update message_builder.py

--- apps/utils/whatsapp/message_builder.py
+++ apps/utils/whatsapp/message_builder.py
@@ -1,1 +1,9 @@
+# 📂 apps/utils/whatsapp/message_builder.py
+from .api_client import WhatsAppClient
 
+client = WhatsAppClient()
+
+def send_otp_to_admin(phone_number, otp_code):
+    # هنا نستدعي قالب واتساب مخصص تم تجهيزه مسبقاً في ميتا باسم 'otp_login'
+    # ملاحظة: سنحتاج لاحقاً لربط المتغيرات (مثل الكود) داخل القالب
+    return client.send_message(phone_number, "otp_login")
