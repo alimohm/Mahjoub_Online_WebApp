f023e85377d021f3e82eabe44499c2d44faa3de2
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-04 14:52:05 +0300

    Update __init__.py

--- apps/statement/__init__.py
+++ apps/statement/__init__.py
@@ -1,15 +1,5 @@
-# coding: utf-8
-# 📂 apps/statement/__init__.py
-
 from flask import Blueprint
 
-# تعريف الـ Blueprint
-statement_blueprint = Blueprint(
-    'statement_blueprint', 
-    __name__, 
-    template_folder='templates', 
-    static_folder='static'
-)
+statement_blueprint = Blueprint('statement_blueprint', __name__, template_folder='templates')
 
-# استيراد الـ routes لتسجيل المسارات
 from . import routes
