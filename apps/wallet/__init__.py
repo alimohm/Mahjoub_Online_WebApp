21f47f57df638fd0ef9e54c2e797ece1aee5a9a8
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-05 22:03:23 +0300

    Update __init__.py

--- apps/wallet/__init__.py
+++ apps/wallet/__init__.py
@@ -1,7 +1,2 @@
-# coding: utf-8
-# 📂 apps/wallet/__init__.py - تهيئة تطبيق المحفظة المستقل
-
-from .routes import wallet_bp
-
-# هذا الملف يجعل المجلد حزمة (Package)، ويسمح باستيراد المسارات 
-# في الملف الرئيسي للتطبيق (apps/__init__.py)
+from apps.wallet.routes import wallet_app
+app.register_blueprint(wallet_app, url_prefix='/wallet')
