2eae300d1197258597a25d652f38d1a481d982f1
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-11 20:19:30 +0300

    Update __init__.py

--- apps/vault/__init__.py
+++ apps/vault/__init__.py
@@ -1,1 +1,12 @@
 
+# 📂 apps/vault/__init__.py
+from flask import Blueprint
+
+vault_bp = Blueprint(
+    'vault_bp', 
+    __name__, 
+    template_folder='templates',
+    static_folder='static'
+)
+
+from apps.vault import routes
