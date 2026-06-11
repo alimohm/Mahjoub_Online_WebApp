4a51bcee518a7155c2500bfffbbd286c5167d724
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-11 21:47:56 +0300

    Update extensions.py

--- apps/extensions.py
+++ apps/extensions.py
@@ -1,11 +1,8 @@
 # 📂 apps/extensions.py
-# الملف المركزي لتعريف إضافات التطبيق (Extensions)
-
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
-from flask_migrate import Migrate  # تم استيراد المكتبة هنا
+from flask_migrate import Migrate
 
-# تعريف كائنات الإضافات
 db = SQLAlchemy()
 login_manager = LoginManager()
-migrate = Migrate()  # تم تعريف كائن المهاجرات هنا ليصبح جاهزاً للاستيراد في __init__.py
+migrate = Migrate()
