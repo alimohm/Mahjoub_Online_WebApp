4fb3dee80632c098fa900b08820e85ffbd1373ca
alimohm <69640611+alimohm@users.noreply.github.com>
2026-06-13 05:11:50 +0900

    Update __init__.py

--- apps/models/__init__.py
+++ apps/models/__init__.py
@@ -1,11 +1,12 @@
 # coding: utf-8
-# 📂 apps/models/__init__.py - مجمع النماذج (النسخة المنظفة والكاملة)
+# 📂 apps/models/__init__.py - مجمع النماذج المحدث
 
 from .admin_db import AdminUser
 from .supplier_db import Supplier
 from .wallet_db import SupplierWallet, WalletTransaction
 from .vault_db import AdminVault, VaultTransaction
 from .financial_db import ExchangeRate, FinancialLog
+from .bridge_db import Product, ProductVariant  # <--- إضافة النماذج الجديدة هنا
 
 __all__ = [
     'AdminUser', 
@@ -15,5 +16,7 @@ __all__ = [
     'AdminVault', 
     'VaultTransaction',
     'ExchangeRate',
-    'FinancialLog'
+    'FinancialLog',
+    'Product',        # <--- إضافة النماذج الجديدة هنا
+    'ProductVariant'  # <--- إضافة النماذج الجديدة هنا
 ]
